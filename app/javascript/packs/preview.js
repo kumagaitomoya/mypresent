9f862c50609bb72039aefde3c3056a8d3288308a
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-12-08 17:55:35 +0900

    投稿画面修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,54 +1,44 @@
-if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function() {
-     const ImageList = document.getElementById('image-list')
- 
-     // 選択した画像を表示する関数
-     const createImageHTML = (blob) => {
-        // 画像を表示するためのdiv要素を生成
-       const imageElement = document.createElement('div')
-       imageElement.setAttribute('class', "image-element")
-      let imageElementNum = document.querySelectorAll('.image-element').length
+if (document.URL.match(/new/) || document.URL.match(/edit/)) {
+  document.addEventListener("DOMContentLoaded", function () {
+    const ImageList = document.getElementById("image-list");
 
- 
-       // 表示する画像を生成
-       const blobImage = document.createElement('img')
-       blobImage.setAttribute('src', blob)
-       blobImage.setAttribute('height', 200);
-       blobImage.setAttribute('width', 200);
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement("div");
+      imageElement.setAttribute("class", "image-element");
+      let imageElementNum = document.querySelectorAll(".image-element").length;
 
-        // ファイル選択ボタンを生成
-      const inputHTML = document.createElement('input')
-      inputHTML.setAttribute('id', `item-image_${imageElementNum}`)
-      inputHTML.setAttribute('name', 'message[images][]')
-      inputHTML.setAttribute('type', 'file')
+      // 表示する画像を生成
+      const blobImage = document.createElement("img");
+      blobImage.setAttribute("src", blob);
+      blobImage.setAttribute("height", 200);
+      blobImage.setAttribute("width", 200);
 
-       // 生成したHTMLの要素をブラウザに表示させる
-       imageElement.appendChild(blobImage)
-       imageElement.appendChild(inputHTML)
-       ImageList.appendChild(imageElement)
-     
-       inputHTML.addEventListener('change', (e) => {
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement("input");
+      inputHTML.setAttribute("id", `item-image_${imageElementNum}`);
+      inputHTML.setAttribute("name", "message[images][]");
+      inputHTML.setAttribute("type", "file");
+
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML);
+      ImageList.appendChild(imageElement);
+
+      inputHTML.addEventListener("change", (e) => {
         file = e.target.files[0];
         blob = window.URL.createObjectURL(file);
 
-        createImageHTML(blob)
-      })
-    }
+        createImageHTML(blob);
+      });
+    };
 
-    document.getElementById('item-image').addEventListener('change', (e) => {
+    document.getElementById("item-image").addEventListener("change", (e) => {
       let file = e.target.files[0];
       let blob = window.URL.createObjectURL(file);
 
-      createImageHTML(blob)
-
+      createImageHTML(blob);
     });
   });
- }
-
-
-
-
-
-
-
-
+}
