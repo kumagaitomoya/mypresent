755d1ed3eaddf1324f1fa13c7914bdcf7b196c62
kumagaitomoya <72239344+kumagaitomoya@users.noreply.github.com>
2020-12-09 18:32:36 +0900

    詳細画面とfc2導入完了

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,8 +1,17 @@
-let map;
-
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+  var mapPosition = { lat: 35.170662, lng: 136.92343 };
+  var mapArea = document.getElementById("maps");
+  var mapOptions = {
+    center: mapPosition,
+    zoom: 16,
+  };
+  var map = new google.maps.Map(mapArea, mapOptions);
+
+  // ↓↓↓ 当記事ではここから解説 ↓↓↓
+  var markerOptions = {
+    map: map,
+    position: mapPosition,
+  };
+  var marker = new google.maps.Marker(markerOptions);
+  // ↑↑↑ 当記事ではここまで解説 ↑↑↑
 }
